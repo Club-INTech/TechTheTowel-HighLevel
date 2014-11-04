aa7566109d0ae28f89b5e926b57d69f2a6a9698a
Pierre Bourré <pierre.bourre@telecom-sudparis.eu>
2014-11-04 18:33:16 +0100

    J'ai oublié ce fichier

---
+++ projects/asservissement/include/Communication.hpp
@@ -0,0 +1,31 @@
+#include "Uart.hpp"
+#include "Singleton.hpp"
+#include "MotionControlSystem.h"
+
+class Communication : public Singleton<Communication> {
+private:
+	typedef Uart<1> serial;
+	MotionControlSystem& motionControlSystem = MotionControlSystem::Instance();
+
+public:
+	Communication() {
+		serial::init(115200);
+	}
+
+	void execute() {
+		if (serial::available()) {
+			char order[200];
+			serial::read(order, 200);
+
+			if(!strcmp("?",order))
+			{
+				serial::printfln("abwabwa");
+			}
+			else if(!strcmp("!",order))
+			{
+				serial::printfln("%lf", 3.15402151024021654);
+			}
+		}
+	}
+
+};
