325a38286a1d78477fa4457886392661347ad6c4
Pierre Bourré <pierre.bourre@telecom-sudparis.eu>
2014-11-05 15:48:36 +0100

    Modification du code asserv' pour une meilleure architecture

--- projects/asservissement/main.cpp
+++ projects/asservissement/main.cpp
@@ -1,10 +1,13 @@
 #include "Communication.hpp"
 #include "delay.h"
 
+namespace {
+	Communication communication;
+}
+
 int main(void)
 {
 	Delay_Init();
-	Communication& communication = Communication::Instance();
 	while(1)
 	{
 		communication.execute();
@@ -18,11 +21,10 @@ void TIM4_IRQHandler(void) { //2kHz = 0.0005s = 0.5ms
 	if (TIM_GetITStatus(TIM4, TIM_IT_Update) != RESET) {
 		//Remise à 0 manuelle du flag d'interruption nécessaire
 		TIM_ClearITPendingBit(TIM4, TIM_IT_Update);
-		//Asservissement
-		Communication& communication = Communication::Instance();
 
-		communication.motionControlSystem.control();
-		communication.motionControlSystem.updatePosition();
+		Communication& communication = Communication::Instance();
+		//Asservissement et mise à jour de la position
+		communication.asservirRobot();
 
 		if (i >= 100) { //50ms
 			communication.motionControlSystem.manageStop();
