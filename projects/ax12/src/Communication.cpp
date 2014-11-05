0fd5515cce22db64754da10feef92fa8155d3202
Pierre Bourré <pierre.bourre@telecom-sudparis.eu>
2014-11-05 15:47:57 +0100

    Modification du code AX 12 pour une meilleure architecture

--- projects/ax12/src/Communication.cpp
+++ projects/ax12/src/Communication.cpp
@@ -1,9 +1,21 @@
-#include "Communication.hpp"
+#include "Communication.h"
 
-class Communication {
-private:
+Communication::Communication(): managerActionneurs(ManagerActionneurs::Instance()) {
+}
 
-public:
-	Communication() {
+void Communication::execute() {
+	char order[100];
+	if (serial_pc::available()) {
+		serial_pc::read(order, 200);
+		if(!strcmp("!", order)) {
+			serial_pc::change_baudrate(9600);
+		}
+		else if(!strcmp("?", order)) {
+			serial_pc::change_baudrate(115200);
+		}
+		else if(!strcmp("a", order)) {
+			serial_pc::disable_rx();
+		}
+		serial_pc::println(order);
 	}
-};
+}
