28b23ee08d385029d2a3134490ceac5891509cdb
paul_t <paul.turpin@telecom-sudparis.eu>
2014-11-20 15:53:11 +0100

    ajout du script de sortie de la zone de depart

---
+++ software/pc/src/scripts/ExitBeginZone.java
@@ -0,0 +1,26 @@
+package scripts;
+
+import smartMath.Vec2;
+import strategie.GameState;
+
+public class ExitBeginZone extends Script {
+
+	@Override
+	public Vec2 point_entree(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int score(int id_version, GameState<?> state) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected void termine(GameState<?> state) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
