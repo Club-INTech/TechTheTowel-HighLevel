9675ca42a5d9fc2e31d66b9837b24007437824fb
Theo_ <theo.carron@tem-tsp.eu>
2015-04-16 16:38:31 +0200

    Setup passé dans le JUnit Test

--- software/pc/src/tests/JUnit_GetPlot.java
+++ software/pc/src/tests/JUnit_GetPlot.java
@@ -55,29 +55,6 @@ public class JUnit_GetPlot extends JUnit_Test
 		real_state.robot.updateConfig();
 	}
 	
-	public void matchSetUp(Robot robot) throws SerialConnexionException
-	{
-		robot.useActuator(ActuatorOrder.ELEVATOR_OPEN_JAW, false);
-
-		robot.useActuator(ActuatorOrder.OPEN_LEFT_GUIDE, false);
-		robot.useActuator(ActuatorOrder.OPEN_RIGHT_GUIDE, true);
-		
-		robot.useActuator(ActuatorOrder.ARM_LEFT_CLOSE, false);
-		robot.useActuator(ActuatorOrder.ARM_RIGHT_CLOSE, false);
-		
-		robot.useActuator(ActuatorOrder.CLOSE_RIGHT_GUIDE, true);
-		robot.useActuator(ActuatorOrder.CLOSE_LEFT_GUIDE, true);
-		
-		robot.useActuator(ActuatorOrder.LEFT_CARPET_FOLDUP, false);
-		robot.useActuator(ActuatorOrder.RIGHT_CARPET_FOLDUP, false);
-		
-		robot.useActuator(ActuatorOrder.LOW_LEFT_CLAP, false);
-		robot.useActuator(ActuatorOrder.LOW_RIGHT_CLAP, false);
-		
-		robot.useActuator(ActuatorOrder.ELEVATOR_CLOSE_JAW, true);
-		
-		robot.useActuator(ActuatorOrder.ELEVATOR_LOW, true);
-	}
 	
 
 	@Test
