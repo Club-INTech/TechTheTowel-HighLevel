ecf74435f4c41e7907a453bb4449c7a71f665fd7
Etienne <etienne.suquet@telecom-sudparis.eu>
2015-04-28 16:24:17 +0200

    ajout classe closerightarmexe

---
+++ software/pc/src/hook/methods/CloseRightArmExe.java
@@ -0,0 +1,38 @@
+package hook.methods;
+import robot.Robot;
+import strategie.GameState;
+import enums.ActuatorOrder;
+import exceptions.serial.SerialConnexionException;
+import hook.Executable;
+
+/**
+ * @author theo
+ * Classe à utiliser par un hook permettant d'ouvrir un bras
+ *
+ */
+
+public class CloseRightArmExe implements Executable
+{
+   
+	 public CloseRightArmExe()
+     {
+			System.out.println("Hook d'ouverture du bras droit");
+     }
+	
+    @Override
+    public boolean execute(GameState<Robot> stateToConsider)
+    {
+    	try 
+    	{
+			System.out.println("en position ("+stateToConsider.robot.getPosition().x+", "+stateToConsider.robot.getPosition().y+") au lancé du hook");
+			stateToConsider.robot.useActuator(ActuatorOrder.ARM_RIGHT_CLOSE, false);
+			System.out.println("en position ("+stateToConsider.robot.getPosition().x+", "+stateToConsider.robot.getPosition().y+") après le lancé du hook");
+		} 
+    	catch (SerialConnexionException e) 
+    	{
+			e.printStackTrace();
+		}
+		return false;
+    }
+        
+}
