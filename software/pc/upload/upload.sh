377cfba04f2cf243590602420733297fd89f29b5
Dede <Dede@Dede.com>
2015-05-07 23:44:53 +0200

    upload script

--- software/pc/upload/upload.sh
+++ software/pc/upload/upload.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 LOGIN=intech
+IPFIXE_RASPI="192.168.1.50"
 DOSSIER_DESTINATION=/home/intech/intech-2015/software/pc/
 
-rsync -e ssh --delete-after --exclude-from exclusion.txt -az ../ "$LOGIN"@"$1":"$DOSSIER_DESTINATION"
+rsync -e ssh --delete-after --exclude-from exclusion.txt -az ../ "$LOGIN"@"$IPFIXE_RASPI":"$DOSSIER_DESTINATION"
