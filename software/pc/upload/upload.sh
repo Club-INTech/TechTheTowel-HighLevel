4cf93ffb8801f94f33f0e20b4122916121ba6748
Dede <dede@plopmail.fr>
2015-05-08 03:37:13 +0200

    upload.sh version singe

--- software/pc/upload/upload.sh
+++ software/pc/upload/upload.sh
@@ -3,6 +3,6 @@ LOGIN=intech
 ADDRESS=192.168.1.50 #Adresse sur la cisco
 #ADDRESS=192.168.43.208 #Adresse sur mon portable
 #ADDRESS=157.159.45.36 #Adresse lorsque sur filaire minet
-DOSSIER_DESTINATION=/home/intech/intech-2015/software/pc
+DESTINATION=/home/intech/intech-2015/software/pc
 
-rsync -e ssh --delete-after --exclude-from=exclusion.txt -az ../ "$LOGIN"@"$ADDRESS":"$DOSSIER_DESTINATION"
+rsync -e ssh --delete-after --exclude-from=exclusion.txt -az ../ "$LOGIN"@"$ADDRESS":"$DESTINATION" ; ssh "$LOGIN"@"$ADDRESS"
