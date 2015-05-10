c0ede26235683feb7bd8ab01cddec614bad7eef6
spraakforskaren <clement.besnier@telecom-sudparis.eu>
2015-05-10 19:59:35 +0200

    Suite de création de tests et modification de locomotion pour les tests

--- software/pc/src/tests/JUnit_Locomotion.java
+++ software/pc/src/tests/JUnit_Locomotion.java
@@ -8,9 +8,14 @@ import java.util.ArrayList;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.Assert;
 
 import enums.ServiceNames;
+//import enums.Direction;
+import exceptions.Locomotion.BlockedException;
 import exceptions.Locomotion.UnableToMoveException;
+import exceptions.Locomotion.UnexpectedObstacleOnPathException;
+import exceptions.serial.SerialConnexionException;
 import robot.DirectionStrategy;
 import robot.Locomotion;
 import robot.cardsWrappers.LocomotionCardWrapper;
@@ -19,7 +24,7 @@ import smartMath.Vec2;
 /**
  * Tests unitaires pour Deplacements.
  *
- * @author pf
+ * @author pf, spraakforskaren
  */
 public class JUnit_Locomotion extends JUnit_Test
 {
@@ -55,6 +60,10 @@ public class JUnit_Locomotion extends JUnit_Test
 		mLocomotion.setPosition(new Vec2 (1381,1000));
 		mLocomotion.setOrientation(Math.PI);
 	}
+	/**
+	 * Ca teste la capacité à avancer et à reculer dans le cas où il n'y a pas d'obstacles, 
+	 * et pas de hooks
+	 */
 
 	@Test
 	public void testMoveLengthwise() 
@@ -64,7 +73,8 @@ public class JUnit_Locomotion extends JUnit_Test
 			int distance = 210;
 			while(true)
 			{
-				cardWrapper.moveLengthwise(distance);
+				mLocomotion.moveLengthwise(distance, null, false);
+				//cardWrapper.moveLengthwise(distance);
 				while(cardWrapper.isRobotMovingAndAbnormal()[0])
 				{
 					if(cardWrapper.isRobotMovingAndAbnormal()[1])
@@ -73,7 +83,7 @@ public class JUnit_Locomotion extends JUnit_Test
 				if(cardWrapper.isRobotMovingAndAbnormal()[1])
 					throw new Exception();
 				
-				cardWrapper.moveLengthwise(-distance);
+				mLocomotion.moveLengthwise(-distance, null, false);
 				while(cardWrapper.isRobotMovingAndAbnormal()[0])
 				{
 					if(cardWrapper.isRobotMovingAndAbnormal()[1])
@@ -96,10 +106,11 @@ public class JUnit_Locomotion extends JUnit_Test
 	/**
 	 * Test_tourner.
 	 * ATTENTION NE FONCTIONNE QUE DU COTE VERT !
+	 * Test ui fait faire des allers-retours
 	 * @throws Exception the exception
 	 */
 	//@Test
-	public void test()
+	public void testFollowPath()
 	{
 		ArrayList<Vec2> path = new ArrayList<Vec2>();
 		path.add(new Vec2 (-500,1000));
@@ -132,5 +143,319 @@ public class JUnit_Locomotion extends JUnit_Test
 			}
 		}
 	}
+	@Test
+	public void testTurn()
+	{
+		try 
+		{
+			mLocomotion.turn(Math.PI/2, null, true);
+		} 
+		catch (UnableToMoveException e) 
+		{
+			log.debug("ça n'a pas bien tourné", this);
+		}
+	}
+	/**On vérifie si on peut aller à un point voulu sans problème vers l'avant en détectant les ennemis
+	 * @throws UnableToMoveException 
+	 * 
+	 */
+	@Test
+	public void testMoveToPointForwardBackward() throws UnableToMoveException
+	{
+		Vec2 position = mLocomotion.getPosition();
+		log.debug("en position : x="+position.x+"; y="+position.y, this);
+		Vec2 aim = new Vec2(0,500);
+		log.debug("position de l'objectif : x="+position.x+"; y="+position.y, this);
+		mLocomotion.JUNIT_moveToPointForwardBackward(aim, null, false, DirectionStrategy.FASTEST, false, true); 
+		Assert.assertEquals(aim, mLocomotion.getPosition());
+	}
+	@Test
+	public void testMoveToPointException() throws UnableToMoveException
+	{
+		Vec2 position = mLocomotion.getPosition();
+		log.debug("en position : x="+position.x+"; y="+position.y, this);
+		Vec2 aim = new Vec2(0,500);
+		log.debug("position de l'objectif : x="+position.x+"; y="+position.y, this);
+		mLocomotion.JUNIT_moveToPointException(aim, null, true, false, false,true); 
+		Assert.assertEquals(aim, mLocomotion.getPosition());
+	}
+	@Test
+	public void testMoveToPointCorrectAngleAndDetectEnnemy() throws UnexpectedObstacleOnPathException, BlockedException, SerialConnexionException
+	{
+		
+		Vec2 position = mLocomotion.getPosition();
+		log.debug("en position : x="+position.x+"; y="+position.y, this);
+		Vec2 aim =  new Vec2(0,500);
+		log.debug("position de l'objectif : x="+position.x+"; y="+position.y, this);
+		mLocomotion.JUNIT_moveToPointCorrectAngleAndDetectEnnemy(aim, null, true, false, true);
+	}
+	@Test
+	public void testCorrectAngle() throws BlockedException, UnexpectedObstacleOnPathException
+	{
+		Vec2 position = mLocomotion.getPosition();
+		log.debug("en position : x="+position.x+"; y="+position.y, this);
+		Vec2 aim = new Vec2(0,500);
+		log.debug("position de l'objectif : x="+position.x+"; y="+position.y, this);
+		mLocomotion.JUNIT_correctAngle(aim, true, true);
+	}
+	/**
+	 * @throws UnexpectedObstacleOnPathException 
+	 * @throws BlockedException 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+	@Test
+	public void testMoveToPointSymmetry() throws BlockedException, UnexpectedObstacleOnPathException
+	{
+		Vec2 position = mLocomotion.getPosition();
+		log.debug("en position : x="+position.x+"; y="+position.y, this);
+		Vec2 aim = new  Vec2(0,500);
+		log.debug("position de l'objectif : x="+position.x+"; y="+position.y, this);
+		
+		mLocomotion.JUNIT_moveToPointSymmetry(aim, true, true, false, false);
+		
+	}
+	/**
+	* Avance, envoi a la serie
+    * @param symmetrisedAim la position visee sur la table (symetrise)
+    * @param givenPosition la position de depart du deplacement
+    * @param angle l'angle dont il faut tourner (ordre pour la serie)
+    * @param distance la distance dont il faut avancer (ordre pour la serie)
+    * @param turnOnly vrai si on veut uniquement tourner (et pas avancer)
+    * @param isCorrection vrai si la consigne est une correction et pas un ordre de deplacement
+    * @throws BlockedException si le robot rencontre un obstacle innatendu sur son chemin (par les capteurs)
+    * @throws UnexpectedObstacleOnPathException 
+    */
+	@Test
+	public void testmoveToPointSerialOrder() throws BlockedException, UnexpectedObstacleOnPathException
+	{
+		Vec2 position = mLocomotion.getPosition();
+		log.debug("en position : x="+position.x+"; y="+position.y, this);
+		Vec2 aim = new Vec2(0,500);
+		log.debug("position de l'objectif : x="+position.x+"; y="+position.y, this);
+		mLocomotion.JUNIT_moveToPointSerialOrder(aim, position, 0, 300, true, false, false);
+	}
+	/**
+	 * 
+	 * @throws BlockedException
+	 * @throws UnexpectedObstacleOnPathException
+	 */
 	
+	@Test
+	public void testMoveToPointSerialOrder() throws BlockedException, UnexpectedObstacleOnPathException
+	{
+		Vec2 position = mLocomotion.getPosition();
+		log.debug("en position : x="+position.x+"; y="+position.y, this);
+		Vec2 aim = new Vec2(0,500);
+		log.debug("position de l'objectif : x="+position.x+"; y="+position.y, this);
+		mLocomotion.JUNIT_moveToPointSerialOrder(aim, position, 0, 300, true, false, false);
+	}
+	/**
+	 * 
+	 * @throws BlockedException
+	 * @throws UnableToMoveException 
+	 */
+	@Test
+	public void	testIsMotionEnded() throws BlockedException, UnableToMoveException 
+	{
+		mLocomotion.moveLengthwise(0, null, false);
+		boolean res1 = mLocomotion.JUNIT_isMotionEnded();
+		Assert.assertEquals(res1, false);
+		mLocomotion.moveLengthwise(1000, null, false);
+		boolean res2 = mLocomotion.JUNIT_isMotionEnded();
+		Assert.assertEquals(res2, true);
+	}
+	@Test 
+	public void testDetectEnemyInDisk(boolean front, boolean isTurnOnly, Vec2 aim) throws UnexpectedObstacleOnPathException 
+	{
+		mLocomotion.detectEnemyInDisk(front, isTurnOnly, aim);
+	}
+	/**
+	 * 
+	 * @param distance
+	 * @param movementDirection
+	 * @throws UnexpectedObstacleOnPathException
+	 * @throws UnableToMoveException 
+	 */
+	@Test
+	public void testDetectEnemyAtDistance(int distance, Vec2 movementDirection) throws UnexpectedObstacleOnPathException, UnableToMoveException 
+	{
+		System.out.println("Placez le robot en ...");
+		System.out.println("Placez un ennemi à une distance inférieure à "+distance);
+		mLocomotion.moveLengthwise(distance+200, null, false);
+		mLocomotion.detectEnemyAtDistance( distance, new Vec2 (0,100));
+		System.out.println("Le robot a dû s'arrêter");
+		
+	}
+	/**
+	 * 
+	 */
+	@Test
+	public void testUpdateCurrentPositionAndOrientation()
+	{
+		mLocomotion.JUNIT_updateCurrentPositionAndOrientation();
+	}
+	/**
+	 * Adaptez le code à vos attentes, il y a trop de possibilités
+	 */
+	@Test
+	public void testUpdateConfig()
+	{
+		mLocomotion.updateConfig();
+		
+	}
+	@Test
+	public void testImmobilise() throws UnableToMoveException
+	{
+		mLocomotion.moveLengthwise(200, null, false);
+		
+		mLocomotion.immobilise();
+		log.debug("Le robot a dû s'arrêter", this);
+	
+	}
+	@Test
+	public void testSetPosition(Vec2 positionWanted)
+	{
+		mLocomotion.setPosition(positionWanted);
+		Vec2 pos = mLocomotion.getPosition();
+		Assert.assertEquals(positionWanted, pos);
+	}
+	/**
+	 * Par la même occasion test getOrientation()
+	 * @param orientation
+	 */
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testSetOrientation(double orientation)
+	{
+		mLocomotion.setOrientation(orientation);
+		double ori = mLocomotion.getOrientation();
+		Assert.assertEquals((float)ori, (float)orientation);
+	}
+	@Test
+	public void testGetPosition()
+	{
+		log.debug("Vérifiez que la position du robot est bien : "+mLocomotion.getPosition(), this);
+	}
+	@Test
+	
+	public void testGetOrientation()
+	{
+		log.debug("Vérifiez que l'orientation du robot est bien : "+mLocomotion.getOrientation(), this);
+	}
+	/**
+	 * A voir avec la série
+	 */
+	@Test 
+	public void testDesasservit()
+	{
+		mLocomotion.desasservit();
+	}
+	/**
+	 * Je ne vois pas où on peut tester le pwm en rotation
+	 * @param pwm
+	 * @throws SerialConnexionException
+	 */
+	@Test
+	public void testSetRotationnalSpeed(int pwm) throws SerialConnexionException
+	{
+		mLocomotion.setRotationnalSpeed(pwm);
+	}
+	/**
+	 * Je vois pas où on peut vérifier le pwm en translation
+	 * @param pwm
+	 * @throws SerialConnexionException
+	 */
+	@Test
+	public void testSetTranslationnalSpeed(int pwm) throws SerialConnexionException
+	{
+		mLocomotion.setTranslationnalSpeed(pwm);
+	}
+	/**
+	 * Faut voir avec la série
+	 * @throws SerialConnexionException
+	 */
+	/*
+	@Test
+	public void testAsservit() throws SerialConnexionException
+	{
+		mLocomotion.asservit();
+	}
+	*/
+	/**
+	 * Fonction qui ne fait rien de Locomotion
+	 */
+	/*
+	@Test
+	public void testInitialiser_deplacements()
+	{
+		mLocomotion.initialiser_deplacements();
+	}
+	*/
+	/**
+	 * Test  difficilement faisable à ce niveau, voir la série
+	 * @throws SerialConnexionException
+	 */
+	/*
+	@Test
+	public void testDisableRotationnalFeedbackLoop() throws SerialConnexionException
+	{
+		mLocomotion.disableRotationnalFeedbackLoop();
+	}
+	*/
+	/**
+	 * Test  difficilement faisable à ce niveau, voir la série
+	 * @throws SerialConnexionException
+	 */
+	/*
+	@Test
+	public void testEnableRotationnalFeedbackLoop() throws SerialConnexionException 
+	{
+		mLocomotion.enableRotationnalFeedbackLoop();
+	}
+	*/
+	/**
+	 * 
+	 * Test  difficilement faisable à ce niveau, voir la série
+	 * 
+	 * @throws SerialConnexionException
+	 */
+	/*
+	@Test 
+	public void testDisableTranslationalFeedbackLoop() throws SerialConnexionException
+	{
+		mLocomotion.disableTranslationalFeedbackLoop();
+		
+	}
+	*/
+	/**
+	 * Test  difficilement faisable à ce niveau, voir la sériev
+	 * @throws SerialConnexionException 
+	 * 
+	 */
+	/*
+	@Test
+	public void testEnableTranslationnalFeedbackLoop() throws SerialConnexionException
+	{
+		mLocomotion.enableTranslationnalFeedbackLoop();
+	}
+	/*
+	/**
+	 * 
+	 * Test de fermeture de la série
+	 * On teste un déplacement et on remarque que le robot n'a pas bougé
+	 */
+	@Test
+	public void testClose()
+	{
+		
+		log.debug("La série est fermée", this);
+		Vec2 beginning = mLocomotion.getPosition();
+		mLocomotion.close();
+		Vec2 end = mLocomotion.getPosition();
+		Assert.assertEquals(beginning, end);
+	}
 }
