4cb1551729158d172709fce25102517fe34963b5
desvigne <julian.desvignes@telecom-sudparis.eu>
2015-09-29 16:36:00 +0200

    Modification pour système de jumper

--- software/arduino/projects/Parasol/Parasol.ino
+++ software/arduino/projects/Parasol/Parasol.ino
@@ -1,6 +1,6 @@
 // Ouverture du parasol (robot secondaire)
-// Relai Moteur PIN  ; interrupteur PIN 8
-// Schéma élec dispo dans un ficher
+// Transistor Moteur PIN 7 ; jumper relié au PIN 8
+
 bool done = 0;
 
 void setup() {
@@ -10,13 +10,13 @@ void setup() {
 }
 
 void loop() {
-   if(digitalRead(8) && !done){ // Dès que l'interrupteur est activé, on lance le compte-à-rebours
+   if(digitalWrite(8) && !done){ // On attend que le jumper soit mis en place (utile pour déterminer un front descendant, duh...)
+     while(digitalWrite(8)){} // On attends le front descendant (enlevage du jumper)
      delay(5000);  // Oui, c'est dégeulasse. A changer par 91000 pour le réel
      digitalWrite(7, HIGH);
      delay(2000); // TODO : A mesurer !!! Ceci est le temps nécessaire à ouvrir le parasol.
      digitalWrite(7, LOW);
      done=1; // Empeche le système de se relancer
- }
-   delay(10);
+   }
   
 }
