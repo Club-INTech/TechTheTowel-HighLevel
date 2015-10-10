2721556989709e6573f7ddba22087a28bb1d8dae
Paul <paul.turpin@telecom-sudgrenoble.eu>
2015-10-10 18:55:36 +0200

    éléments de correction

--- software/pc/src/scripts/CloseDoors.java
+++ software/pc/src/scripts/CloseDoors.java
@@ -20,17 +20,17 @@ import utils.Log;
 /**
  * @author julian
  * Script pour la fermeture des portes des cabines
- * Version 0 : Déplacement de la serviette aux portes puis fermeture ; aucune action prévue hors du déplacement ; aucun pathdingding ; si pb -> arrêt complet
+ * Version 0 : Deplacement de la serviette aux portes puis fermeture ; aucune action prevue hors du deplacement ; aucun pathdingding ; si pb -> arret complet
  */
 public class CloseDoors extends AbstractScript
 {
 	/**
-	 * Distance en x signée entre le centre de la serviette et le centre des portes, a modifier une fois les dimensions du robot connues
+	 * Distance en x signee entre le centre de la serviette et le centre des portes, a modifier une fois les dimensions du robot connues
 	 */
 	private int xBetweenEntryAndDoors = -300;
 	
 	/**
-	 * Distance en y signée entre le centre de la serviette et le centre des portes, a modifier une fois les dimensions du robot connues
+	 * Distance en y signee entre le centre de la serviette et le centre des portes, a modifier une fois les dimensions du robot connues
 	 */
 	private int yBetweenEntryAndDoors = 850;
 
@@ -45,7 +45,7 @@ public class CloseDoors extends AbstractScript
 	
 	/**
 	 * On lance le script choisi.
-	 * @param versionToExecute Version à lancer
+	 * @param versionToExecute Version a lancer
 	 * @param stateToConsider Notre bon vieux robot
 	 * @param hooksToConsider Les hooks necessaires pour l'execution du script
 	 */
@@ -56,16 +56,18 @@ public class CloseDoors extends AbstractScript
 		{
 			try
 			{
+				//TODO si on modifie la vitesse il faut la remettre � la fin
 				stateToConsider.robot.setLocomotionSpeed(Speed.BETWEEN_SCRIPTS_SLOW);
-				//On vérifie l'orientation du robot à pi, sinon on le tourne
+				//TODO rien ne sert de verifier, on tourne
+				//On verifie l'orientation du robot a pi, sinon on le tourne
 				if(Math.abs(stateToConsider.robot.getOrientation() - Math.PI) >= (Math.PI / 6))
 				{
 					stateToConsider.robot.turn(Math.PI);
 				}
-			
-				//On déplace le robot sans chercher à savoir s'il y a un obstacle (début de match, impossible qu'il y ait l'adversaire a moins qu'il ne possède un portail warp)
+				//TODO on ne suppose jamais que c'est l debut du match, la strategie decidera
+				//On deplace le robot sans chercher à savoir s'il y a un obstacle (debut de match, impossible qu'il y ait l'adversaire a moins qu'il ne possede un portail warp)
 				stateToConsider.robot.moveLengthwiseWithoutDetection(xBetweenEntryAndDoors, hooksToConsider, false);
-			
+				//si tu veux vraiment etre sur fait un do -> while mais sinon si le robot est bloqu� �coute le plutot que de lui redemander la meme chose
 				//Verification de la position en x et correction
 				if(Math.abs(stateToConsider.robot.getPosition().x - 1050) >= 50)
 				{
@@ -75,16 +77,17 @@ public class CloseDoors extends AbstractScript
 				//On tourne le robot vers les portes
 				stateToConsider.robot.turn((Math.PI / 2), hooksToConsider, false);
 				
-				//Vérification de l'orientation et correction
+				//TODO toutes ces verification son innutiles, les m�thodes sont bloquantes (le code s'arrete tant que tu n'est pas arriv�) et renvoie des exeption en cas de probleme
+				//Verification de l'orientation et correction
 				if((Math.abs(stateToConsider.robot.getOrientation() - (Math.PI /2)) >= (Math.PI / 6)))
 				{
 					stateToConsider.robot.turn((Math.PI / 2), hooksToConsider, false);
 				}
 			
-				//On déplace le robot vers les portes, sans rentrer dans le tas
+				//On deplace le robot vers les portes, sans rentrer dans le tas
 				stateToConsider.robot.moveLengthwise((yBetweenEntryAndDoors-100), hooksToConsider, false);
-			
-				//Vérification et correction
+				//TODO idem
+				//Verification et correction
 				if(Math.abs(stateToConsider.robot.getPosition().y) >= 50)
 				{
 					stateToConsider.robot.moveLengthwise((stateToConsider.robot.getPositionFast().y - (yBetweenEntryAndDoors-100)), hooksToConsider, false);
@@ -97,12 +100,15 @@ public class CloseDoors extends AbstractScript
 				stateToConsider.robot.moveLengthwiseWithoutDetection(-30, hooksToConsider, false);
 				
 				//PORTES FERMEES !
+				//TODO il faut modifier l'�tat de la table !!!
+				//et le nombre de de points r�alis�es dans GameState
 			}
 			catch(UnableToMoveException e)
 			{
 				finalize(stateToConsider);
 				throw new ExecuteException(e);
 			}
+			//TODO else
 			
 		}
 	}
@@ -129,12 +135,13 @@ public class CloseDoors extends AbstractScript
 	@Override
 	public void finalize(GameState<?> state) throws SerialFinallyException
 	{
-		
+		//TODO
 		
 	}
 
 	@Override
-	public Integer[] getVersion(GameState<?> stateToConsider) {
+	public Integer[] getVersion(GameState<?> stateToConsider) 
+	{
 		return versions;
 	}
 	
