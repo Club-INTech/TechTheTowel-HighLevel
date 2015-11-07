3ba1215c3ed9a754d9b9b7c7c9ffb2d2ad3367d0
julian <julian.desvignes@telecom-sudparis.eu>
2015-11-07 20:46:37 +0100

    JUnit modifié pour test sur robot

--- software/pc/src/tests/JUnit_TextualPDDTest.java
+++ software/pc/src/tests/JUnit_TextualPDDTest.java
@@ -6,19 +6,24 @@ import org.junit.*;
 import org.junit.runner.JUnitCore;
 
 import pathDingDing.*;
+import robot.RobotReal;
 import smartMath.Vec2;
 import table.Table;
 import utils.Log;
 import enums.*;
-
+import exceptions.PathNotFoundException;
+import exceptions.Locomotion.UnableToMoveException;
 import graphics.Window;
+import hook.Hook;
 
 public class JUnit_TextualPDDTest extends JUnit_Test
 {	
 	Window win;
 	Table table;
 	PathDingDing pf;
+	RobotReal robot;
 	Log log;
+	long time;
 	
 
 	public static void main(String[] args) throws Exception
@@ -32,12 +37,14 @@ public class JUnit_TextualPDDTest extends JUnit_Test
         super.setUp();
         log = (Log)container.getService(ServiceNames.LOG);
         table = (Table)container.getService(ServiceNames.TABLE);
+        robot = (RobotReal)container.getService(ServiceNames.ROBOT_REAL);
         pf = (PathDingDing)container.getService(ServiceNames.PATHDINGDING);
     }
     
     @Test
     public void test()
     {
+    	time = System.currentTimeMillis();
     	ArrayList<Node> path = pf.computePath(new Vec2(0, 1750), new Vec2(-300, 1000));
     	ArrayList<Vec2> pathVec = new ArrayList<Vec2>();
     	for(int i=0 ; i<path.size() ; i++)
@@ -45,6 +52,13 @@ public class JUnit_TextualPDDTest extends JUnit_Test
     		pathVec.add(path.get(i).getPosition());
     		log.debug(path.get(i).getPosition());
     	}
+    	log.debug(System.currentTimeMillis()-time);
+    	/*
+    	try {
+			robot.moveToLocation(new Vec2(-300, 1000), new ArrayList<Hook>(), table);
+		} catch (PathNotFoundException | UnableToMoveException e) {
+			e.printStackTrace();
+		} */
     	
     }
     
