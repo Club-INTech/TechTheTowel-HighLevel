81145c3bdaecbb6f0ce5749c002dfb08bd1ba5d5
Cerezas <ch.cyril_@hotmail.fr>
2015-11-15 18:47:36 +0100

    corrections script parasol

--- software/pc/src/scripts/Parasol.java
+++ software/pc/src/scripts/Parasol.java
@@ -23,9 +23,9 @@ import utils.Log;
 
 public class Parasol extends AbstractScript
 {
-	// Etat du parasol ; overkill
-	//TODO passer en documentation + plutot dans robot
-	private static boolean opened = false;
+	// TODO passer en documentation + plutot dans robot (concernant l'état du parasol)
+	// je n'ai pas très bien compris ce que tu veux dire ;
+	// j'ai créé un static parasolUnfolded dans la classe GameState pour connaître l'état du parasol, est-ce bon ?
 	
 	public Parasol(HookFactory hookFactory, Config config, Log log)
 	{
@@ -42,33 +42,42 @@ public class Parasol extends AbstractScript
 	@Override
 	public void execute(int versionUnused, GameState<Robot> actualState, ArrayList<Hook> hooksToConsider) throws SerialFinallyException, ExecuteException
 	{
-		try
+		if (actualState.getTimeEllapsed() >= (long)90 && !actualState.getIsParasolUnfolded())
 		{
-			// envoi du message d'ouverture du parasol au bas niveau
-			actualState.robot.useActuator(ActuatorOrder.OPEN_PARASOL, true);
-			// actualisation de l'état du parasol
-			Parasol.opened = true;
+			try
+			{
+				// envoi du message d'ouverture du parasol au bas niveau
+				actualState.robot.useActuator(ActuatorOrder.OPEN_PARASOL, true);
+				// actualisation de l'état du parasol maintenant déployé
+				actualState.parasolUnfolded();
 			
+			}
+			catch(SerialConnexionException e)
+			{
+				finalize(actualState);
+				throw new ExecuteException(e);
+			}
 		}
-		catch(SerialConnexionException e)
+		else
 		{
-			finalize(actualState);
-			throw new ExecuteException(e);
+			// TODO throws new exception à définir si la méthode est appelée alors que le temps écoulé n'est pas 90s ?
 		}
 	}
 	
-	//TODO le score varie en foction de la position du parassol et du temps restant dans le match
+	//TODO le score varie en fonction de la position du parasol et du temps restant dans le match
+	// objection : le parasol est la funny action qui ne fait pas partie de la stratégie des 90s
 	@Override
 	public int remainingScoreOfVersion(int version, GameState<?> state)
 	{
-		return 0;
+		return 20;
 	}
 
-	//TODO retourer la position du robot (comme ca pas de deplacement)
+
 	@Override
 	public Circle entryPosition(int version, int ray, Vec2 robotPosition) 
 	{
-		return new Circle(new Vec2(0,0));
+		// retour de la position actuelle du robot afin que celui-ci ne se déplace pas
+		return new Circle(robotPosition, 10);
 	}
 
 	@Override
@@ -78,7 +87,6 @@ public class Parasol extends AbstractScript
 		
 	}
 
-	//TODO
 	@Override
 	public Integer[] getVersion(GameState<?> stateToConsider) 
 	{
