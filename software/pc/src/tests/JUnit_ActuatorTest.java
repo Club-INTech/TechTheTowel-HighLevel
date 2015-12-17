d52acc16d04a83ff401ce65750b58760cff49dab
discord <julian.desvignes@telecom-sudparis.eu>
2015-12-17 10:50:22 +0100

    JUnit test ax12

---
+++ software/pc/src/tests/JUnit_ActuatorTest.java
@@ -0,0 +1,38 @@
+package tests;
+
+import enums.ActuatorOrder;
+import enums.ServiceNames;
+import exceptions.serial.SerialConnexionException;
+import hook.Hook;
+import org.junit.Before;
+import org.junit.Test;
+import robot.Robot;
+import strategie.GameState;
+
+import java.util.ArrayList;
+
+public class JUnit_ActuatorTest extends JUnit_Test
+{
+    private GameState<Robot> mRobot;
+    private ArrayList<Hook> emptyHook = new ArrayList<Hook>();
+
+    @Before
+    public void setUp() throws Exception
+    {
+        super.setUp();
+        mRobot = (GameState<Robot>)container.getService(ServiceNames.GAME_STATE);
+        mRobot.updateConfig();
+        mRobot.robot.setAX12Speed(12);
+    }
+
+    @Test
+    public void go() throws SerialConnexionException {
+        mRobot.robot.useActuator(ActuatorOrder.FISHING_POSITION_LEFT, true);
+        mRobot.robot.useActuator(ActuatorOrder.FISHING_POSITION_RIGHT, true);
+        mRobot.robot.useActuator(ActuatorOrder.MIDLE_POSITION_LEFT, true);
+        mRobot.robot.useActuator(ActuatorOrder.MIDLE_POSITION_RIGHT, true);
+        mRobot.robot.useActuator(ActuatorOrder.FREE_FISHES_LEFT, true);
+        mRobot.robot.useActuator(ActuatorOrder.FREE_FISHES_RIGHT, true);
+
+    }
+}
