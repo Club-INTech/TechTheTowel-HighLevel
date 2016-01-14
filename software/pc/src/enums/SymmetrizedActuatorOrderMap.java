11fe47a3fd6c6d97e271920f78735ba5548a556e
discord <julian.desvignes@telecom-sudparis.eu>
2016-01-14 15:34:22 +0100

    symétrie des actionneurs

--- software/pc/src/enums/SymmetrizedActuatorOrderMap.java
+++ software/pc/src/enums/SymmetrizedActuatorOrderMap.java
@@ -18,9 +18,12 @@ public class SymmetrizedActuatorOrderMap
      */
     public SymmetrizedActuatorOrderMap()
     {
-    	//TODO associer un actionneur a son symetrique sur le robot (gauche droit)
-    	//exemple : mCorrespondenceMap.put(ActuatorOrder.LEFT_CARPET_DROP, ActuatorOrder.RIGHT_CARPET_DROP);
-
+    	mCorrespondenceMap.put(ActuatorOrder.FISHING_POSITION_LEFT, ActuatorOrder.FISHING_POSITION_RIGHT);
+        mCorrespondenceMap.put(ActuatorOrder.MIDDLE_POSITION_LEFT, ActuatorOrder.MIDDLE_POSITION_RIGHT);
+        mCorrespondenceMap.put(ActuatorOrder.LEFT_MAGNET_DOWN, ActuatorOrder.RIGHT_MAGNET_DOWN);
+        mCorrespondenceMap.put(ActuatorOrder.LEFT_MAGNET_UP, ActuatorOrder.RIGHT_MAGNET_UP);
+        mCorrespondenceMap.put(ActuatorOrder.LEFT_FINGER_DOWN, ActuatorOrder.RIGHT_FINGER_DOWN);
+        mCorrespondenceMap.put(ActuatorOrder.LEFT_FINGER_UP, ActuatorOrder.RIGHT_FINGER_UP);
     }
     
     /**
