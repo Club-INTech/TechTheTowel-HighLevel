d3afb222481fb454a0ebf5d06f91798e4b284dcc
CF <francois.charles@telecom-sudparis.eu>
2016-01-31 17:57:34 +0100

    avec les hooks c'est mieux

---
+++ software/pc/src/hook/methods/GetFish.java
@@ -0,0 +1,22 @@
+package hook.methods;
+
+import enums.ActuatorOrder;
+import exceptions.serial.SerialConnexionException;
+import hook.Executable;
+import robot.Robot;
+import strategie.GameState;
+
+public class GetFish implements Executable
+{
+
+    @Override
+    public boolean execute(GameState<Robot> stateToConsider) {
+        try {
+            stateToConsider.robot.useActuator(ActuatorOrder.FISHING_POSITION, true);
+
+        } catch (SerialConnexionException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
---
+++ software/pc/src/hook/types/HookXGreater.java
@@ -0,0 +1,37 @@
+package hook.types;
+
+import hook.Hook;
+import robot.RobotReal;
+import strategie.GameState;
+import utils.Config;
+import utils.Log;
+
+/**
+ * Hook s'activant quand le robot va au dessus d'une certaine abscisse
+ * @author CF
+ */
+public class HookXGreater extends Hook
+{
+
+    private int XValue;
+
+    /**
+     * ce constructeur ne sera appellé que par les constructeurs des classes filles (des hooks bien précis)
+     *
+     * @param config    endroit ou lire la configuration du robot
+     * @param log       Système de log sur lequel écrire
+     * @param gameState Etat du jeu sur lequel on vérifie si le hook se déclenche ou non
+     */
+    public HookXGreater(Config config, Log log, GameState<RobotReal> gameState, int XValue)
+    {
+        super(config, log, gameState);
+        this.XValue = XValue;
+    }
+
+    @Override
+    public boolean evaluate() {
+        if(mState.robot.getPosition().x > XValue)
+            return trigger();
+        return false;
+    }
+}
