62508419601bb5d59dd068b2aea757d567116ea3
desvigne <julian.desvignes@telecom-sudparis.eu>
2016-03-23 17:32:19 +0100

    ShellGetter

--- software/pc/src/scripts/ShellGetter.java
+++ software/pc/src/scripts/ShellGetter.java
@@ -187,7 +187,7 @@ public class ShellGetter extends AbstractScript
                 // on se place pour repartir
                 stateToConsider.robot.moveLengthwise(200);*/
 
-                stateToConsider.robot.moveArc(new Arc(900, -400, stateToConsider.robot.getOrientationFast(), false), hooksToConsider);
+                stateToConsider.robot.moveArc(new Arc(200, -400, stateToConsider.robot.getOrientationFast(), false), hooksToConsider);
 
                 // on reprend la vitesse pre-script
                 stateToConsider.robot.setLocomotionSpeed(speedBeforeScriptWasCalled);
