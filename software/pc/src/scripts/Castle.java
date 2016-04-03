871d440f6ca202517a6a9d6e8e75897dcab4310b
desvigne <julian.desvignes@telecom-sudparis.eu>
2016-04-03 17:54:16 +0200

    castle

--- software/pc/src/scripts/Castle.java
+++ software/pc/src/scripts/Castle.java
@@ -250,11 +250,11 @@ public class Castle extends AbstractScript
 			else if(versionToExecute == 3)
 			{
                 Speed speedBeforeScriptWasCalled = stateToConsider.robot.getLocomotionSpeed();
-                stateToConsider.robot.setLocomotionSpeed(Speed.SLOW_ALL);
+                stateToConsider.robot.setLocomotionSpeed(Speed.MEDIUM_ALL);
 
 				stateToConsider.robot.setForceMovement(true);
 
-				stateToConsider.robot.moveArc(new Arc(-460, 950, stateToConsider.robot.getOrientation(), false), hooksToConsider);
+				stateToConsider.robot.moveArc(new Arc(-490, 950, stateToConsider.robot.getOrientation(), false), hooksToConsider);
 
                 stateToConsider.robot.turn(Math.PI);
 				
@@ -284,7 +284,7 @@ public class Castle extends AbstractScript
 				// partie bloquante pour fermer les cabines sans lancer l'appel à close doors
 				try
 				{
-					stateToConsider.robot.setLocomotionSpeed(Speed.MEDIUM_ALL);
+					//stateToConsider.robot.setLocomotionSpeed(Speed.MEDIUM_ALL);
 					stateToConsider.robot.setForceMovement(true);
 					Arc arc = new Arc(-1200, -1100, stateToConsider.robot.getOrientation(), false);
 					stateToConsider.robot.moveArc(arc, hooksToConsider);
