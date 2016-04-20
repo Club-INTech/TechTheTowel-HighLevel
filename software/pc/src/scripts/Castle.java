576c1a185e393ca2271eaf1ecad10da1680b9e97
desvigne <julian.desvignes@telecom-sudparis.eu>
2016-04-20 13:38:45 +0200

    plus proche

--- software/pc/src/scripts/Castle.java
+++ software/pc/src/scripts/Castle.java
@@ -339,7 +339,7 @@ public class Castle extends AbstractScript
 		if (version == 0)
 		{
 			// former: return (new Circle(new Vec2(1200, 980)));
-			return (new Circle(new Vec2(1050,1120)));
+			return (new Circle(new Vec2(1050,1000)));
 		}
 		else if (version == 1)
 		{
