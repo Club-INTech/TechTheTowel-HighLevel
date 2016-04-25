c8d9f45bb6e6dd70bdbf43170fd16265e456008e
CF <francois.charles@telecom-sudparis.eu>
2016-04-25 19:29:15 +0200

    tests

--- software/pc/src/tests/JUnit_Hooks.java
+++ software/pc/src/tests/JUnit_Hooks.java
@@ -1,12 +1,21 @@
 package tests;
 
+import enums.ActuatorOrder;
 import enums.ServiceNames;
+import enums.Speed;
+import exceptions.ContainerException;
+import exceptions.Locomotion.UnableToMoveException;
 import exceptions.serial.SerialConnexionException;
+import exceptions.serial.SerialManagerException;
+import hook.Callback;
 import hook.Hook;
+import hook.methods.SpeedDown;
 import hook.types.HookFactory;
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import robot.Robot;
-import robot.cardsWrappers.SensorsCardWrapper;
+import robot.RobotReal;
 import scripts.ScriptManager;
 import strategie.GameState;
 import table.Table;
@@ -20,33 +29,46 @@ import java.util.ArrayList;
 
 public class JUnit_Hooks extends JUnit_Test 
 {
-	ArrayList<Hook> emptyHook;
-	GameState<Robot> real_state;
-	ScriptManager scriptmanager;
-	SensorsCardWrapper  mSensorsCardWrapper;
-	HookFactory hookFactory;
+	private GameState<Robot> theRobot;
+	private ScriptManager scriptManager;
+	private HookFactory hookFactory;
+	private ArrayList<Hook> emptyHook = new ArrayList<Hook>();
 	
 	@SuppressWarnings("unchecked")
 	@Before
 	public void setUp() throws Exception
 	{
 		super.setUp();
-		real_state = (GameState<Robot>) container.getService(ServiceNames.GAME_STATE);
-		scriptmanager = (ScriptManager) container.getService(ServiceNames.SCRIPT_MANAGER);
-		mSensorsCardWrapper = (SensorsCardWrapper) container.getService(ServiceNames.SENSORS_CARD_WRAPPER);
-
-
-		real_state.robot.setPosition(Table.entryPosition);
-		real_state.robot.setOrientation(Math.PI);
-		
-		real_state.robot.updateConfig();
+		scriptManager = (ScriptManager)container.getService(ServiceNames.SCRIPT_MANAGER);
+		theRobot = (GameState<Robot>)container.getService(ServiceNames.GAME_STATE);
+		hookFactory = (HookFactory) container.getService(ServiceNames.HOOK_FACTORY);
+		theRobot.robot.useActuator(ActuatorOrder.ARM_INIT, true);
+		theRobot.robot.setOrientation(Math.PI);
+		theRobot.robot.setPosition(Table.entryPosition);
+		theRobot.robot.setLocomotionSpeed(Speed.MEDIUM_ALL);
+	}
+	
+	@Test
+	public void testSpeed()
+	{
+		Hook speed = hookFactory.newXLesserHook(1000);
+		speed.addCallback(new Callback(new SpeedDown(),true,theRobot));
+		emptyHook.add(speed);
+		log.debug("Début de test !");
 		try 
 		{
-			matchSetUp(real_state.robot, false);
-		} 
-		catch (SerialConnexionException e) 
+			theRobot.robot.moveLengthwise(500,emptyHook,false);
+		}
+		catch (UnableToMoveException e) 
 		{
-			log.debug( e.logStack());
-		}		
+			log.debug("Haha, fat chance !");
+		}
+	}
+	
+	@After
+	public void after()
+	{
+		theRobot.robot.immobilise();
+		log.debug("Fin de test !");
 	}
 }
