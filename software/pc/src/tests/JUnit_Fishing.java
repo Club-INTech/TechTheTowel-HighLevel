ec77fe22d79906c45075f83ec53a1eff6f2f5e7b
desvigne <julian.desvignes@telecom-sudparis.eu>
2016-04-28 01:39:02 +0200

    fishing junit

--- software/pc/src/tests/JUnit_Fishing.java
+++ software/pc/src/tests/JUnit_Fishing.java
@@ -88,8 +88,8 @@ public class JUnit_Fishing extends JUnit_Test
 	{
         try 
         {
-			theRobot.table.getObstacleManager().freePoint(scriptManager.getScript(ScriptNames.FISHING).entryPosition(3, theRobot.robot.getRobotRadius(), theRobot.robot.getPosition()).position);
-            scriptManager.getScript(ScriptNames.FISHING).goToThenExec(3, theRobot, emptyHook);
+			theRobot.table.getObstacleManager().freePoint(scriptManager.getScript(ScriptNames.FISHING).entryPosition(4, theRobot.robot.getRobotRadius(), theRobot.robot.getPosition()).position);
+            scriptManager.getScript(ScriptNames.FISHING).goToThenExec(4, theRobot, emptyHook);
         } catch (Exception e) 
         {
             e.printStackTrace();
