da90726c96eaa04c9b4f24abec8be648da11499b
desvigne <julian.desvignes@telecom-sudparis.eu>
2016-05-02 20:24:11 +0200

    un poil plus bourrin

--- software/pc/src/scripts/TechTheSand.java
+++ software/pc/src/scripts/TechTheSand.java
@@ -254,6 +254,8 @@ public class TechTheSand extends AbstractScript
 				}
 				try {
 					stateToConsider.robot.turn(Math.PI);
+
+					stateToConsider.robot.moveLengthwise(50);
 				} catch (Exception e)
 				{
 					e.printStackTrace();
