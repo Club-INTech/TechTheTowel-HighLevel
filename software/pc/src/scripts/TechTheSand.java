8b2edddfbb40e175b88fbd5dd3b1ba87f4581eaa
desvigne <julian.desvignes@telecom-sudparis.eu>
2016-05-02 21:53:47 +0200

    moins loin

--- software/pc/src/scripts/TechTheSand.java
+++ software/pc/src/scripts/TechTheSand.java
@@ -135,7 +135,7 @@ public class TechTheSand extends AbstractScript
 
 				//Arc start = new Arc(stateToConsider.robot.getPosition(), entry.start, entry.startAngle, true);
 
-				Arc start = new Arc(-200, 60, Math.PI, false);
+				Arc start = new Arc(-200, 50, Math.PI, false);
 
 				//stateToConsider.robot.setForceMovement(true);
 
