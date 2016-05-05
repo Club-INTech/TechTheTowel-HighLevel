65288cb5e1a9f0366852cefe607a13389b4eb28a
desvigne <julian.desvignes@telecom-sudparis.eu>
2016-05-05 18:43:32 +0200

    corrections

--- software/pc/src/scripts/TechTheSand.java
+++ software/pc/src/scripts/TechTheSand.java
@@ -494,7 +494,7 @@ public class TechTheSand extends AbstractScript
                 ThreadSensor.modeBorgne(true);
               //  stateToConsider.robot.useActuator(ActuatorOrder.OPEN_DOOR, true);
                 stateToConsider.robot.turnWithoutDetection(Math.PI/2, hooksToConsider);
-                stateToConsider.robot.moveLengthwiseWithoutDetection((2000-330) - stateToConsider.robot.getPosition().y);
+                stateToConsider.robot.moveLengthwiseWithoutDetection((2000-350) - stateToConsider.robot.getPosition().y);
 
                 stateToConsider.robot.turnWithoutDetection(Math.PI, hooksToConsider);
                 stateToConsider.robot.setBasicDetection(true);
